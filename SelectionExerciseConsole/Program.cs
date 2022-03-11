41506195da1397f5420bd5ff81672462b57340b9
PlinioAbner <plinio414@gmail.com>
2022-03-11 10:27:15 -0500

    initial commit

---
+++ SelectionExerciseConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SelectionExerciseConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             int favoriteNumber = 497;
+
+             Console.WriteLine("Guess the favorite number, Hint: below 500");
+             int userGuess = int.Parse(Console.ReadLine());
+
+             if (userGuess < favoriteNumber)
+             {
+             Console.WriteLine("Too Low!!!");
+             }
+             else if (userGuess > favoriteNumber)
+             {
+            Console.WriteLine("Too High!!!");
+             }
+            else if (userGuess == favoriteNumber)
+             {
+            Console.WriteLine("You were correct here is a cookie");
+             }
+             else
+             {
+            Console.WriteLine("Nevermind");
+             }
+
+            
+            
+        }
+    }
+}
